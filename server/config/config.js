e59f8ed7fd8f58c0c4988acf9442ea8746a9d3dd
Pablo Gonzales <pablogonzales.218@gmail.com>
2020-05-05 20:30:29 -0300

    variable de entorno heroku

--- server/config/config.js
+++ server/config/config.js
@@ -17,7 +17,7 @@ if(process.env.NODE_ENV === 'dev'){
     urlDB = 'mongodb://localhost:27017/cafe'
 }
 else{
-    urlDB = 'mongodb+srv://Pablo:Wx3ON6uANP5ThksD@cluster0-q2ikz.mongodb.net/Cafe'
+    urlDB = process.env.MONGO_URI  
 }
 
 process.env.urlDB = urlDB  // asignamos al objeto global process la propiedad urlDB
